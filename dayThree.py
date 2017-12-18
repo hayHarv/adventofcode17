d6a8336bf9075c8c70b2104b01043704942dda2b
hayHarv <hsharvey@gmail.com>
2017-12-18 13:27:55 -0800

    finish part 1 of day 3

--- dayThree.py
+++ dayThree.py
@@ -34,13 +34,51 @@ def find_previous(x):
 
 def which_box(x):
     if x == 1:
-        return 1
+        max_val = 1
+        box = 1
+        return box
     else:
-        return len(find_previous(x))+1
+        previous = find_previous(x)
+        max_val = max(previous)
+        box = len(previous) +1
+        return box
 
-#TODO find coordinates
-#TODO check for efficiency on find_previous...seems shitty
+def sidelength(nth_box):
+    return last_digit(nth_box)**.5
 
-if __name__=="__main__":
-    assert which_box(10) == 3
-    assert which_box(26) == 4
+def start_coords(x):
+    base = (which_box(x))-1
+    x_coord = base
+    y_coord = base*-1
+    return [x_coord, y_coord]
+
+
+def squares_in_box(nth_box):
+    sidelens = sidelength(nth_box)
+    return int((sidelens*4)-4)
+
+def midsides(x):
+    a = which_box(x) -1
+    a_zero = pow(a*2 -1, 2)
+    return a, [a_zero + x * a for x in [1,3,5,7]]
+
+def distance(x):
+    a, centers = midsides(x)
+    return a + min([abs(x - i) for i in centers])
+
+def next_coordinates(x, y):
+    if x == y == 0: return (1,0)
+    if y > -x and x > y: return (x, y+1)
+    if y > -x and y >= x: return(x-1, y)
+    if y <= -x and x < y : return (x, y-1)
+    if y <= -x and x >= y: return(x+1, y)
+
+
+if  __name__=="__main__":
+    assert which_box(10) ==  3
+    assert which_box(26) ==  4
+    assert last_digit(2) == 9
+    assert sidelength(2) == 3
+    assert sidelength(1) == 1
+    assert start_coords(10) == [2, -2]
+    assert start_coords(1) == [0,0]
